8133c38fccb9764a2b4947dc89034fc8be0ffe30
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2024-11-30 14:58:21 +0330

    sync for games update

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -472,13 +472,15 @@ class ApiController extends Controller
         ]);
 
         try {
-            if ($tableName === 'games') {
-                $query = "SELECT * FROM {$tableName} WHERE id = ? AND status = 0 AND deleted_at IS NULL";
-                $data = DB::connection('useraccount')->select($query, [$id]);
-            } else {
-                $query = "SELECT * FROM {$tableName} WHERE id = ?";
-                $data = DB::connection('useraccount')->select($query, [$id]);
-            }
+            // if ($tableName === 'games') {
+            //     $query = "SELECT * FROM {$tableName} WHERE id = ? AND status = 0 AND deleted_at IS NULL";
+            //     $data = DB::connection('useraccount')->select($query, [$id]);
+            // } else {
+            //     $query = "SELECT * FROM {$tableName} WHERE id = ?";
+            //     $data = DB::connection('useraccount')->select($query, [$id]);
+            // }
+            $query = "SELECT * FROM {$tableName} WHERE id = ?";
+            $data = DB::connection('useraccount')->select($query, [$id]);
             return response()->json($data);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Database connection failed: ' . $e->getMessage()], 500);
