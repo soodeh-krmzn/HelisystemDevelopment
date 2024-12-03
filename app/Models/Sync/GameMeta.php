ff8683e9aabfdc018fe8771fa6ed78f5f0d70d34
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2024-12-03 10:44:27 +0330

    Update GameMeta.php

--- app/Models/Sync/GameMeta.php
+++ app/Models/Sync/GameMeta.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
-class Game extends Sync
+class GameMeta extends Sync
 {
     use HasFactory, SoftDeletes;
 
