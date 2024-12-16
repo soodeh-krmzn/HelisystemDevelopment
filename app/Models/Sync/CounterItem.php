2b69e8e71ca179805bdd93777a3d160580c177ca
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2024-12-16 11:39:36 +0330

    Update CounterItem.php

--- app/Models/Sync/CounterItem.php
+++ app/Models/Sync/CounterItem.php
@@ -4,12 +4,11 @@ namespace App\Models\Sync;
 
 use App\Models\MyModels\Sync;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class CounterItem extends Sync
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $guarded = [];
 
