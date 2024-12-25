79fe71fcaa8a6ff201b635d591ce25cd7fe0501a
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2024-12-25 13:40:49 +0330

    Update license.blade.php

--- resources/views/account/license.blade.php
+++ resources/views/account/license.blade.php
@@ -35,7 +35,6 @@
                                                     <th>وضعیت</th>
                                                     <th>وضعیت فعال</th>
                                                     <th>کاربر فعال</th>
-                                                    {{-- <th>عملیات</th> --}}
                                                 </tr>
                                             </thead>
                                             <tbody>
@@ -44,7 +43,6 @@
                                                         <td>{{ $loop->index + 1 }}</td>
                                                         <td class="scrollable">{{ $license->license }}</td>
                                                         <td>{{ $license->systemCode }}</td>
-                                                        {{-- <td>{{ $license->status == 0 ? 'غیر فعال' : 'فعال' }}</td> --}}
                                                         <td>
                                                             <button
                                                                 class="btn btn-sm license-status-btn {{ $license->status ? 'btn-success' : 'btn-danger' }}"
@@ -53,33 +51,9 @@
                                                                 {{ $license->status ? 'فعال' : 'غیرفعال' }}
                                                             </button>
                                                         </td>
-                                                        <td>{{ $license->isActive == 0 ? 'خاموش' : 'در حال استفاده' }}</td>
+                                                        <td class="badge bg-{{ $license->isActive == 0 ? 'secondary' : 'success'}}">{{ $license->isActive == 0 ? 'خاموش' : 'در حال استفاده' }}</td>
                                                         <td>{{ optional(findUser($license->userActive))->getFullName() }}
                                                         </td>
-                                                        {{-- <td id="status-td"
-                                                            class="{{ $license->status == 0 ? 'bg-danger' : 'bg-success' }} text-center">
-                                                            <select name="status" id="status" class="form-control"
-                                                                data-account_id="{{ $license->account->id }}"
-                                                                data-license_id="{{ $license->id }}">
-                                                                <option value="1"
-                                                                    {{ $license->status == 1 ? 'selected' : '' }}>فعال
-                                                                </option>
-                                                                <option value="0"
-                                                                    {{ $license->status == 0 ? 'selected' : '' }}>غیرفعال
-                                                                </option>
-                                                            </select>
-                                                        </td> --}}
-                                                        {{-- <td>
-                                                            <a href="{{ route('user.changePassword', $user->id) }}"
-                                                                class="btn btn-secondary btn-sm"><i
-                                                                    class="fa fa-key"></i></a>
-                                                            <a href="{{ route('user.edit', $user) }}"
-                                                                class="btn btn-warning btn-sm"><i
-                                                                    class="fa fa-pencil"></i></a>
-                                                            <a href="{{ route('user.destroy', $user) }}"
-                                                                class="btn btn-danger btn-sm" data-confirm-delete="true"><i
-                                                                    class="fa fa-trash"></i></a>
-                                                        </td> --}}
                                                     </tr>
                                                 @endforeach
                                             </tbody>
@@ -106,7 +80,6 @@
                 const currentStatus = button.data("license-status");
                 const newStatus = currentStatus === 1 ? 0 : 1;
 
-                // SweetAlert2 confirmation
                 Swal.fire({
                     title: 'آیا مطمئن هستید؟',
                     text: `وضعیت لایسنس به ${newStatus ? 'فعال' : 'غیرفعال'} تغییر خواهد کرد.`,
@@ -118,7 +91,6 @@
                     cancelButtonText: 'لغو'
                 }).then((result) => {
                     if (result.isConfirmed) {
-                        // AJAX request to update the status
                         $.ajax({
                             type: "POST",
                             url: `/account-license-status/${licenseId}`,
@@ -127,7 +99,6 @@
                                 _token: $('meta[name="csrf-token"]').attr('content'),
                             },
                             success: function(response) {
-                                // Update the button text and class
                                 button.data("license-status", newStatus);
                                 button.text(newStatus ? 'فعال' : 'غیرفعال');
                                 button
