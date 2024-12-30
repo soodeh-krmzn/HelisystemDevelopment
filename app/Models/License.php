fdac7b737be83164b420f659cebdd90e41d50e20
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2024-12-30 08:48:18 +0330

    soft delete to license

--- app/Models/License.php
+++ app/Models/License.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class License extends Model
 {
-    use HasFactory;
+    use HasFactory ,SoftDeletes;
     protected $table = 'licenses';
     protected $fillable = ['account_id', 'license' , 'status' , 'systemCode'];
 
