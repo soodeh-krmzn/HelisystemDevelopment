3ebd63e2c94367e534676d0f7c6b99dd43279fe5
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2025-01-19 10:45:12 +0330

    avoid duplicate

--- app/Http/Middleware/ApiLogMiddleware.php
+++ app/Http/Middleware/ApiLogMiddleware.php
@@ -19,18 +19,24 @@ class ApiLogMiddleware
     {
         $response = $next($request);
 
-        ApiLog::create([
-            'endpoint' => $request->path(),
-            'method' => $request->method(),
-            'account_id' => $request->attributes->get('account_id'),
-            'user_name' => $request->attributes->get('username'),
-            'request_data' => json_encode($request->all(), JSON_UNESCAPED_UNICODE),
-            'response_data' => json_encode(json_decode($response->getContent(), true), JSON_UNESCAPED_UNICODE),
-            'status_code' => $response->getStatusCode(),
-            'ip_address' => $request->ip(),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        if (!ApiLog::where('endpoint', $request->path())
+            ->where('method', $request->method())
+            ->where('created_at', '>=', now()->subSeconds(1)) // جلوگیری از لاگ مشابه در یک ثانیه
+            ->exists()) {
+            ApiLog::create([
+                'endpoint' => $request->path(),
+                'method' => $request->method(),
+                'account_id' => $request->attributes->get('account_id'),
+                'user_name' => $request->attributes->get('username'),
+                'request_data' => json_encode($request->all(), JSON_UNESCAPED_UNICODE),
+                'response_data' => json_encode(json_decode($response->getContent(), true), JSON_UNESCAPED_UNICODE),
+                'status_code' => $response->getStatusCode(),
+                'ip_address' => $request->ip(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
 
         return $response;
     }
--- app/Http/Middleware/SetUser.php
+++ app/Http/Middleware/SetUser.php
@@ -18,15 +18,12 @@ class SetUser
      */
     public function handle(Request $request, Closure $next)
     {
-        // استخراج اطلاعات کاربر از درخواست (مثال: username, licenseKey و ...)
         $username = $request->input('username');
         $licenseKey = $request->input('licenseKey');
 
-        // یافتن کاربر و حساب مرتبط
         $user = User::where('username', $username)->first();
         $accountId = $user?->account_id;
 
-        // ذخیره اطلاعات در attributes
         $request->attributes->set('account_id', $accountId);
         $request->attributes->set('username', $username);
 
