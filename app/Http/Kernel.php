23b2c766d38ae27ec82eb073883a9f9377ba6efc
soodeh-karamoozian <soodeh.karamoozian@gmail.com>
2025-01-20 07:45:26 +0330

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\SetLocale::class,
-        \App\Http\Middleware\ApiLogMiddleware::class,
     ];
 
     /**
